ec41ba2a6e2daf9004f4137b3293cc2e2e86b47d
JorimJoram <wkdgyfla97@gmail.com>
2024-10-08 12:14:45 +0900

    database connect

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, UploadFile, HTTPException, status
+from fastapi import FastAPI, UploadFile, HTTPException, status, Depends
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 import uuid
 
-from model.User import User
+from model.User import User as user
+from model.models import Base, User
+from database import engine, get_db
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(root_path="/api/v1", docs_url="/api/docs")
 
@@ -32,5 +37,15 @@ async def create_code():
     return uuid.uuid4()
 
 @app.post("/create/user", tags=["user_info"])
-async def create_user_info(request: User):
-    return User
+async def create_user_info(request: user, db:Session = Depends(get_db)):
+    # db_user = db.query(User).filter(User.code == user.code).first()
+    
+    # if db_user:
+    #     raise HTTPException(status_code=400, detail="code was already registered")
+
+    new_user = User(code = user.code, gender = user.gender, age = user.age)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
