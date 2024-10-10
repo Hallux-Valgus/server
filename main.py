44f6b7fa27019af2e0552bcb4c986207da51e74b
JorimJoram <wkdgyfla97@gmail.com>
2024-10-10 11:52:57 +0900

    fetch - file received!

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI, UploadFile, HTTPException, status, Depends
+from fastapi import FastAPI, UploadFile,File,Form, HTTPException, status, Depends
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
+import logging
 import uuid
 
 from Service.User.UserService import UserService
 from model.Requests.User import UserRequest
 
 user_service = UserService()
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s \n%(levelname)s: \t %(message)s")
 
 app = FastAPI(root_path="/api/v1", docs_url="/api/docs")
 
@@ -26,7 +29,7 @@ class LoginData(BaseModel):
     password: str
 
 
-@app.post("/login", tags=["auth"])
+@app.post("/login", tags=["admin"])
 async def test_login_response(data: LoginData):
     if data.username == "test" and data.password == "1234":
         return {"message": "Login Success"}
@@ -38,15 +41,23 @@ async def test_login_response(data: LoginData):
 async def create_code():
     return uuid.uuid4()
 
+
 @app.get("/get/user", tags=["user_info"])
-async def get_user_by_code(code:str):
+async def get_user_by_code(code: str):
     return user_service.read_by_code(code)
 
+
 @app.post("/create/user", tags=["user_info"])
-async def create_user_info(request:UserRequest):
+async def create_user_info(request: UserRequest):
     return user_service.create_user(request)
 
 
+@app.post("/upload", tags=["image"])
+async def upload_image(image:UploadFile = File(...), code:str=Form(...)):
+    logging.info(f"{image.filename} - {code}")
+    return {"image": image.filename, "code": code}
+
+
 @app.get("/test", tags=["tmp_test"])
-async def test_page(code:str):
+async def test_page(code: str):
     return user_service.read_by_code(code)
